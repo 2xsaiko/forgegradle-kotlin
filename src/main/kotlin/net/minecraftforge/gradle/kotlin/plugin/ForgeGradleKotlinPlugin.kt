6d1375e6461fe3a1fdedbd4180f3be5f3dc1712b
therealfarfetchd <git@marco-rebhan.de>
2018-05-14 23:03:27 +0200

    Plugin should automatically apply FG plugin

--- src/main/kotlin/net/minecraftforge/gradle/kotlin/plugin/ForgeGradleKotlinPlugin.kt
+++ src/main/kotlin/net/minecraftforge/gradle/kotlin/plugin/ForgeGradleKotlinPlugin.kt
@@ -1,10 +1,11 @@
 package net.minecraftforge.gradle.kotlin.plugin
 
+import net.minecraftforge.gradle.plugin.ForgeGradlePlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class ForgeGradleKotlinPlugin : Plugin<Project> {
-  override fun apply(target: Project) {
-    // not sure we need to do anything in this?
+  override fun apply(project: Project) {
+    project.pluginManager.apply(ForgeGradlePlugin::class.java)
   }
 }
